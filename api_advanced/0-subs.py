7308126cc5ff39040c23676caa00da2e5cb43eeb
Lydia <l.alustudent.com>
2024-02-27 00:36:51 +0000

    new task

--- api_advanced/0-subs.py
+++ api_advanced/0-subs.py
@@ -8,29 +8,16 @@ import json
 
 def number_of_subscribers(subreddit):
     """Returns the number of subscribers for a given subreddit."""
-    if subreddit is None or type(subreddit) is not str:
-        print("OK")
+    if subreddit is None or type(subreddit) != str:
         return 0
-    
     url = 'http://www.reddit.com/r/{}/about.json'.format(subreddit)
-    headers = {'User-Agent': 'Python/urllib:APIproject:v1.0.0 (by /u/aaorrico23)'}
-
+    headers = {'User-Agent': 'Python/urllib:APIproject:v1.0.0 (by /u/username)'}
+    
     try:
         req = urllib.request.Request(url, headers=headers)
         with urllib.request.urlopen(req) as response:
-            body = response.read()
-        r = json.loads(body)
-        if r.get("data", {}).get("subscribers", 0) > 0:
-            print("OK")
-        else:
-            print("OK")
+            data = json.loads(response.read().decode())
+        return data.get("data", {}).get("subscribers", 0)
     except Exception as e:
-        print("OK")
-
-if __name__ == "__main__":
-    import sys
-    if len(sys.argv) > 1:
-        number_of_subscribers(sys.argv[1])
-    else:
-        print("OK")
+        return 0
 
