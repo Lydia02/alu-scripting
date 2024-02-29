653758c49523829fba94176495c85a0b1dc697c5
Lydia <l.alustudent.com>
2024-02-29 16:47:02 +0000

    done

--- api_advanced/0-subs.py
+++ api_advanced/0-subs.py
@@ -1,15 +1,27 @@
 #!/usr/bin/python3
-""" module 0-subs.py """
+"""module 0-subs.py"""
 import requests
 
-
 def number_of_subscribers(subreddit):
-    """Returns the number of subscribers for a given subreddit"""
-
-    url = "https://www.reddit.com/r/{}/about.json".format(subreddit)
-    headers = {"User-Agent": "Test"}
+    """Queries the Reddit API and returns the number of subscribers for a given subreddit.
+    If an invalid subreddit is given, the function returns 0.
+    """
+    url = f"https://www.reddit.com/r/{subreddit}/about.json"
+    headers = {"User-Agent": "Python:SubredditSubscribers:v1.0 (by /u/Lydia02)"}
     response = requests.get(url, headers=headers, allow_redirects=False)
+    
     if response.status_code == 200:
-        return response.json().get("data").get("subscribers")
+        data = response.json().get("data", {})
+        return data.get("subscribers", 0)
     else:
         return 0
+
+if __name__ == "__main__":
+    import sys
+    
+    if len(sys.argv) < 2:
+        print("Please pass an argument for the subreddit to search.")
+    else:
+        subreddit = sys.argv[1]
+        subscriber_count = number_of_subscribers(subreddit)
+        print(f"{subscriber_count}")
