73f3b70f53d9e12ed74d4bae7f2761f35f309df0
Ojoawo Lydia Subuola <l.ojoawo@alustudent.com>
2024-02-29 18:50:24 +0100

    Update 0-subs.pyv

--- api_advanced/0-subs.py
+++ api_advanced/0-subs.py
@@ -1,22 +1,18 @@
 #!/usr/bin/python3
-"""
-0-subs
-"""
-
-import requests
+# get subs
+from requests import get
+from sys import argv
 
 
 def number_of_subscribers(subreddit):
-    """Returns the number of subscribers for a given subreddit."""
-    url = 'https://www.reddit.com/r/{}/about.json'.format(subreddit)
-    headers = \
-        {'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7)\
-          AppleWebKit/537.36(KHTML, like Gecko) \
-         Chrome/90.0.4430.93 Safari/537.36'}
-
-    response = requests.get(url, headers=headers, allow_redirects=False)
-
-    if response.status_code == 200:
-        return response.json().get('data').get('subscribers')
-    else:
+    """subs"""
+    head = {'User-Agent': 'Dan Kazam'}
+    count = get('https://www.reddit.com/r/{}/about.json'.format(
+        subreddit), headers=head).json()
+    try:
+        return count.get('data').get('subscribers')
+    except:
         return 0
+
+if __name__ == "__main__":
+    number_of_subscribers(argv[1])
