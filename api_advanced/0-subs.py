72dc57b4034c68b8ff898df57234c39882af7ea4
Lydia <l.alustudent.com>
2024-02-29 16:49:46 +0000

    done

--- api_advanced/0-subs.py
+++ api_advanced/0-subs.py
@@ -1,27 +1,32 @@
 #!/usr/bin/python3
-"""module 0-subs.py"""
+"""
+Returns the number of subscribers from a subreddit
+"""
 import requests
 
+
 def number_of_subscribers(subreddit):
-    """Queries the Reddit API and returns the number of subscribers for a given subreddit.
-    If an invalid subreddit is given, the function returns 0.
-    """
-    url = f"https://www.reddit.com/r/{subreddit}/about.json"
-    headers = {"User-Agent": "Python:SubredditSubscribers:v1.0 (by /u/Lydia02)"}
-    response = requests.get(url, headers=headers, allow_redirects=False)
-    
+    """ Set a custom header user-agent """
+    headers = {"User-Agent": "ALU-scripting API 0.1"}
+    url = "https://www.reddit.com/r/{}.json".format(subreddit)
+
+    try:
+        response = requests.get(url, headers=headers,
+                                timeout=30, allow_redirects=False)
+
+    except requests.exceptions.Timeout:
+        return "The request Timed out"
+
     if response.status_code == 200:
-        data = response.json().get("data", {})
-        return data.get("subscribers", 0)
+        json_data = response.json()
+        subscriber_number = (
+            json_data.get("data")
+            .get("children")[0]
+            .get("data")
+            .get("subreddit_subscribers")
+        )
+        return subscriber_number
+    elif response.status_code == 404:
+        return 0
     else:
         return 0
-
-if __name__ == "__main__":
-    import sys
-    
-    if len(sys.argv) < 2:
-        print("Please pass an argument for the subreddit to search.")
-    else:
-        subreddit = sys.argv[1]
-        subscriber_count = number_of_subscribers(subreddit)
-        print(f"{subscriber_count}")
