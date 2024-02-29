32b5660927668b08af5c17103ea12f5ddc42be2e
Ojoawo Lydia Subuola <l.ojoawo@alustudent.com>
2024-02-29 19:13:50 +0100

    Update 0-subs.pyk

--- api_advanced/0-subs.py
+++ api_advanced/0-subs.py
@@ -1,21 +1,16 @@
 #!/usr/bin/python3
-"""0-subs
-
-Run ./0-main.py programming or ./0-main.py this_is_a_fake_subreddit for testing
-"""
-
+"""Function to query subscribers on a given Reddit subreddit."""
 import requests
 
 
 def number_of_subscribers(subreddit):
-    """Function that queries the Reddit API and returns the number of
-    subscribers (not active users, total subscribers) for a given subreddit.
-    If an invalid subreddit is given, the function should return 0.
-    """
+    """Return the total number of subscribers on a given subreddit."""
     url = "https://www.reddit.com/r/{}/about.json".format(subreddit)
-    headers = {"User-Agent": "0-subs.py"}
+    headers = {
+        "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/122.0.0.0 Safari/537.36"
+    }
     response = requests.get(url, headers=headers, allow_redirects=False)
-    if response.status_code == 200:
-        return response.json().get("data").get("subscribers")
-    else:
+    if response.status_code == 404:
         return 0
+    results = response.json().get("data")
+    return results.get("subscribers")
