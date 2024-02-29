cbb75520d1fe133e9b7c423c77148cb49f0641fa
Lydia <l.alustudent.com>
2024-02-29 17:11:49 +0000

    done

--- api_advanced/0-subs.py
+++ api_advanced/0-subs.py
@@ -1,29 +1,33 @@
 #!/usr/bin/python3
 """
-Script that prints 'OK' if the number_of_subscribers function executes successfully.
+Returns the number of subscribers from a subreddit.
 """
 import requests
 import sys
 
+
 def number_of_subscribers(subreddit):
-    """Set a custom header user-agent"""
+    """Set a custom header user-agent."""
     headers = {"User-Agent": "ALU-scripting API 0.1"}
     url = "https://www.reddit.com/r/{}.json".format(subreddit)
 
     try:
-        response = requests.get(url, headers=headers, timeout=30, allow_redirects=False)
+        response = requests.get(url, headers=headers,
+                                timeout=30, allow_redirects=False)
     except requests.exceptions.Timeout:
-        print("The request Timed out")
+        print("The request timed out.")
         return None  # Indicates that an exception occurred
 
     if response.status_code == 200:
         json_data = response.json()
-        subscriber_number = json_data.get("data").get("children")[0].get("data").get("subreddit_subscribers")
+        data_path = json_data.get("data").get("children")[0].get("data")
+        subscriber_number = data_path.get("subreddit_subscribers")
         return subscriber_number
     elif response.status_code == 404:
         return 0
     else:
         return 0
+
 
 if __name__ == "__main__":
     if len(sys.argv) < 2:
@@ -31,7 +35,7 @@ if __name__ == "__main__":
     else:
         result = number_of_subscribers(sys.argv[1])
         if result is not None:
-            print("OK")  # This will print 'OK' for both existing and non-existing subreddits as long as the function does not raise an exception.
+            print("OK")  # This will print 'OK' for both...
         else:
             print("Error executing function.")
 
