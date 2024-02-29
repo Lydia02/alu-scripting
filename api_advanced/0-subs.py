f9d28032d145288220fd3f4ffe97ada6588a8fce
Lydia <l.alustudent.com>
2024-02-29 17:19:00 +0000

    done

--- api_advanced/0-subs.py
+++ api_advanced/0-subs.py
@@ -1,41 +1,21 @@
 #!/usr/bin/python3
-"""
-Returns the number of subscribers from a subreddit.
-"""
-import requests
-import sys
+"""A function that queries the Reddit API and
+ returns the number of subscribers (all subscribers)"""
 
 
 def number_of_subscribers(subreddit):
-    """Set a custom header user-agent."""
-    headers = {"User-Agent": "ALU-scripting API 0.1"}
-    url = "https://www.reddit.com/r/{}.json".format(subreddit)
-
-    try:
-        response = requests.get(url, headers=headers,
-                                timeout=30, allow_redirects=False)
-    except requests.exceptions.Timeout:
-        print("The request timed out.")
-        return None  # Indicates that an exception occurred
-
-    if response.status_code == 200:
-        json_data = response.json()
-        data_path = json_data.get("data").get("children")[0].get("data")
-        subscriber_number = data_path.get("subreddit_subscribers")
-        return subscriber_number
-    elif response.status_code == 404:
+    """
+    queries the Reddit API
+    returns the number of subscribers for a given reddit
+    """
+    import json
+    import requests
+    subreddit_URL = 'https://www.reddit.com/r/{}/about/.json'.format(
+        subreddit)
+    subreddit_info = requests.get(subreddit_URL,
+                                  headers={"user-agent": "user"},
+                                  allow_redirects=False).json()
+    if "data" not in subreddit_info:
         return 0
-    else:
-        return 0
-
-
-if __name__ == "__main__":
-    if len(sys.argv) < 2:
-        print("Please pass an argument for the subreddit to search.")
-    else:
-        result = number_of_subscribers(sys.argv[1])
-        if result is not None:
-            print("OK")  # This will print 'OK' for both...
-        else:
-            print("Error executing function.")
-
+    subscribers = subreddit_info.get("data").get("subscribers")
+    return subscribers
