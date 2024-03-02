f7ecffe78371c15eee1b0231682464617ea013e7
Ojoawo Lydia Subuola <l.ojoawo@alustudent.com>
2024-03-02 15:03:52 +0100

    Update 0-subs.pyj

--- api_advanced/0-subs.py
+++ api_advanced/0-subs.py
@@ -1,17 +1,19 @@
 #!/usr/bin/python3
-"""
-Contains the number_of_subscribers function
-"""
-
 import requests
 
-
 def number_of_subscribers(subreddit):
-    """returns the number of subscribers for a given subreddit"""
-    if subreddit is None or type(subreddit) is not str:
+    """Fetch the number of subscribers for a given subreddit."""
+    url = f"https://www.reddit.com/r/{subreddit}/about.json"
+    headers = {'User-Agent': 'Custom User Agent'}
+    response = requests.get(url, headers=headers, allow_redirects=False)
+    
+    if response.status_code == 200:
+        try:
+            data = response.json()
+            return data['data']['subscribers']
+        except (ValueError, KeyError):
+            # In case the response does not contain the expected keys or is not valid JSON
+            return 0
+    else:
+        # If the status code is not 200, the subreddit is likely invalid or another error occurred
         return 0
-    r = requests.get('http://www.reddit.com/r/{}/about.json'.format(subreddit),
-                     headers={'User-Agent': 'Python/requests:APIproject:\
-v1.0.0 (by /u/subuola)'}).json()
-    subs = r.get("data", {}).get("subscribers", 0)
-    return subs
