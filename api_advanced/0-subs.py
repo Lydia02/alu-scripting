459559c8a087a0882c7f9cbc861caa16d82e0667
Ojoawo Lydia Subuola <l.ojoawo@alustudent.com>
2024-03-02 14:55:04 +0100

    Update 0-subs.pyj

--- api_advanced/0-subs.py
+++ api_advanced/0-subs.py
@@ -1,20 +1,17 @@
+#!/usr/bin/python3
+"""
+Contains the number_of_subscribers function
+"""
+
 import requests
 
+
 def number_of_subscribers(subreddit):
-    """Returns the number of subscribers for a given subreddit."""
-    if not isinstance(subreddit, str):
+    """returns the number of subscribers for a given subreddit"""
+    if subreddit is None or type(subreddit) is not str:
         return 0
-    
-    url = 'https://www.reddit.com/r/{}/about.json'.format(subreddit)
-    headers = {'User-Agent': 'Python/requests:APIproject:v1.0.0 (by /u/subuola)'}
-    
-    try:
-        response = requests.get(url, headers=headers)
-        # Check if the subreddit was not found or other client/server errors
-        if response.status_code == 404 or response.status_code >= 400:
-            return 0
-        
-        data = response.json()
-        return data.get("data", {}).get("subscribers", 0)
-    except requests.RequestException:
-        return 0
+    r = requests.get('http://www.reddit.com/r/{}/about.json'.format(subreddit),
+                     headers={'User-Agent': 'Python/requests:APIproject:\
+v1.0.0 (by /u/subuola)'}).json()
+    subs = r.get("data", {}).get("subscribers", 0)
+    return subs
