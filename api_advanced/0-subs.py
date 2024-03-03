c63063b09ce2eed73109eba51b6788a6a1aa2aa4
Ojoawo Lydia Subuola <l.ojoawo@alustudent.com>
2024-03-03 19:19:47 +0100

    Update 0-subs.pyyy

--- api_advanced/0-subs.py
+++ api_advanced/0-subs.py
@@ -1,21 +1,19 @@
 #!/usr/bin/python3
-"""A function that queries the Reddit API and
- returns the number of subscribers (all subscribers)"""
+"""
+Script that queries subscribers on a given Reddit subreddit.
+"""
+
+import requests
 
 
 def number_of_subscribers(subreddit):
-    """
-    queries the Reddit API
-    returns the number of subscribers for a given reddit
-    """
-    import json
-    import requests
-    subreddit_URL = 'https://www.reddit.com/r/{}/about/.json'.format(
-        subreddit)
-    subreddit_info = requests.get(subreddit_URL,
-                                  headers={"user-agent": "user"},
-                                  allow_redirects=False).json()
-    if "data" not in subreddit_info:
+    """Return the total number of subscribers on a given subreddit."""
+    url = "https://www.reddit.com/r/{}/about.json".format(subreddit)
+    headers = {"User-Agent": "Mozilla/5.0"}
+    response = requests.get(url, headers=headers, allow_redirects=False)
+    if response.status_code == 200:
+        data = response.json()
+        subscribers = data['data']['subscribers']
+        return subscribers
+    else:
         return 0
-    subscribers = subreddit_info.get("data").get("subscribers")
-    return subscribers
